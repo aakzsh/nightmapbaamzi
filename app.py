3656574debb558b78815426b63284896093ab0f2
shruti <shrutigupta5555@gmail.com>
2021-10-21 15:52:53 +0530

    firebase image upload

--- app.py
+++ app.py
@@ -6,8 +6,21 @@ import re
 from io import BytesIO, StringIO
 import cv2
 
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore, storage
+import uuid
+
+cred = credentials.Certificate("key.json")
+firebase_admin.initialize_app(cred, {
+    'storageBucket': 'awestruck-d86c7.appspot.com'
+})
+
 app = Flask(__name__)
 app.config["DEBUG"] = True
+
+db = firestore.client()
+bucket = storage.bucket()
 
 @app.route('/', methods = ['GET'])
 def index():        
@@ -19,8 +32,14 @@ def image():
     image_b64 = request.values['imageBase64']
     
     image_PIL = Image.open(BytesIO(base64.b64decode(image_b64.split(",")[1])))
-    image_PIL.save('output/20.png')
+    img_id = str(uuid.uuid4())
+    image_PIL.save(f"{img_id}.png")
 
+    blob = bucket.blob(f"{img_id}.png")
+    imgpath = f"E:\\nightmapbaamzi\\{img_id}.png"
+    with open(imgpath, 'rb') as my_file:
+        blob.upload_from_file(my_file)
+    print(blob.public_url)
     return ''
 
 
