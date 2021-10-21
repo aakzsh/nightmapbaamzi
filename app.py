9a0a6d386711fd198ab5c794783216e7d5d4981a
shruti <shrutigupta5555@gmail.com>
2021-10-21 11:03:33 +0530

    storing image via post req in flask

--- app.py
+++ app.py
@@ -1,11 +1,34 @@
 from flask import Flask, render_template, request
+import numpy as np
+from PIL import Image
+import base64
+import re
+from io import BytesIO, StringIO
+import cv2
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+@app.route('/', methods = ['GET'])
+def index():        
+    return render_template('index.html', lat = 30, lng = 70)
 
+@app.route('/image', methods = ['POST'])
+def image():
+    print("here")
+    image_b64 = request.values['imageBase64']
+    
+    image_PIL = Image.open(BytesIO(base64.b64decode(image_b64.split(",")[1])))
+    image_PIL.save('output/10.png')
+
+    return ''
+
+
+@app.route('/get_image', methods = ['GET'])
+def get_image():
+    
+
+    return ''
+   
 if __name__ == "__main__":
     app.run(debug=True)
