8a94df7440d716b4e78c520ad1cb35519fc0ee3e
aakzsh <aakashferrari@gmail.com>
2021-10-25 12:52:45 +0530

    add image to firestore

--- app.py
+++ app.py
@@ -20,34 +20,50 @@ app = Flask(__name__)
 app.config["DEBUG"] = True
 
 db = firestore.client()
+
+
 bucket = storage.bucket()
 
-@app.route('/', methods = ['GET'])
-def index():        
-    return render_template('index.html', lat = 30, lng = 70)
+@app.route('/code=<code>', methods = ['GET'])
+def index(code): 
+    print(code)       
+    return render_template('index.html', lat = 30, lng = 70, code=code)
 
 @app.route('/image', methods = ['POST'])
 def image():
     print("here")
     image_b64 = request.values['imageBase64']
-    
-    image_PIL = Image.open(BytesIO(base64.b64decode(image_b64.split(",")[1])))
-    img_id = str(uuid.uuid4())
-    image_PIL.save(f"{img_id}.png")
+    # print(image_b64)
+    doc_ref = db.collection(u'room').document(u'HEHEHE') #add the dynamic code here
+    doc_ref.update({
+    u'url': str(image_b64).split(",")[1],
+    })
+    print("success")
+    # image_PIL = Image.open(BytesIO(base64.b64decode(image_b64.split(",")[1])))
+    # img_id = str(uuid.uuid4())
+    # image_PIL.save(f"{img_id}.png")
 
-    blob = bucket.blob(f"{img_id}.png")
-    imgpath = f"E:\\nightmapbaamzi\\{img_id}.png"
-    with open(imgpath, 'rb') as my_file:
-        blob.upload_from_file(my_file)
-    print(blob.public_url)
+    # blob = bucket.blob(f"{img_id}.png")
+    # imgpath = f"E:\\nightmapbaamzi\\{img_id}.png"
+    # with open(imgpath, 'rb') as my_file:
+    #     blob.upload_from_file(my_file)
+    # print(blob.public_url)
+
     return ''
 
 
 @app.route('/get_image', methods = ['GET'])
 def get_image():
     image_id = request.form.get('image_id')
-
     return ''
+
+@app.route('/walk_stats/<name>/<steps_today>/<total_steps>/<level>')
+def walk_stats(name,steps_today,total_steps,level):
+    return render_template('walk_stats.html', name= name, steps_today=steps_today, total_steps=total_steps,level=level)
    
+@app.route('/meditation_stats/<name>/<total_time>')
+def meditation_stats(name,total_time):
+    return render_template('meditation_stats.html', name= name, total_time=total_time)
+
 if __name__ == "__main__":
     app.run(debug=True, threaded = True)
